52fb2846f9bdab6ed5a01f26f83025ec6205c2ac
Huitzoo <eckobik@gmail.com>
2018-10-20 13:29:05 -0500

    init apms-server

---
+++ APMS/src/main/java/com/apms/ApmsApplication.java
@@ -0,0 +1,12 @@
+package com.apms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApmsApplication.class, args);
+	}
+}
