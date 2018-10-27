2ca270a051b859e7b087b51239a5c2d537d1a056
Huitzoo <eckobik@gmail.com>
2018-10-27 04:06:56 -0500

    fixes

--- APMS/src/main/java/com/apms/ApmsApplication.java
+++ APMS/src/main/java/com/apms/ApmsApplication.java
@@ -2,9 +2,14 @@ package com.apms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ApmsApplication {
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApmsApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApmsApplication.class, args);
