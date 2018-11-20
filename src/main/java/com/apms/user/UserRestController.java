024de4d6a1b7bd60bf427b6871a087d73a8434dd
DavidJChavez <davidjchavez98@gmail.com>
2018-11-20 00:00:36 -0600

    Cambio en metodos REST de User

--- src/main/java/com/apms/user/UserRestController.java
+++ src/main/java/com/apms/user/UserRestController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.apms.humanResource.HumanResourceService;
 import com.apms.rest.RESTRequest;
 import com.apms.rest.RESTResponse;
 
@@ -22,6 +23,9 @@ public class UserRestController {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private HumanResourceService humanResourceService;
 
 	/*
 	 ** Return a listing of all the resources
@@ -70,6 +74,7 @@ public class UserRestController {
 		try {
 			if(userService.getOne(user.getPayload().getId()) != null)
                 return new RESTResponse<User>(RESTResponse.FAIL, "Usuario ya existe en el sistema.", null);
+			humanResourceService.add(user.getPayload().getHumanResource());
 			userService.add(user.getPayload());
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -85,6 +90,7 @@ public class UserRestController {
 	@PatchMapping
 	public RESTResponse<User> patch(@RequestBody RESTRequest<User> user) {
 		try {
+			humanResourceService.update(user.getPayload().getHumanResource());
 			userService.update(user.getPayload());
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -100,6 +106,7 @@ public class UserRestController {
 	@PutMapping
 	public RESTResponse<User> put(@RequestBody RESTRequest<User> user) {
 		try {
+			humanResourceService.update(user.getPayload().getHumanResource());
 			userService.update(user.getPayload());
 		} catch (Exception e) {
 			e.printStackTrace();
