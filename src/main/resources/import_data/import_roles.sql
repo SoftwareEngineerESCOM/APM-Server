59670ef85c61933e62cf7dcd445f44c000942c85
akotadi <manuel_7795@hotmail.com>
2018-11-20 18:04:10 -0600

    Modulating import_data sql

---
+++ src/main/resources/import_data/import_roles.sql
@@ -0,0 +1,6 @@
+-- add roles
+insert into role (description, name, rank) values ('Encargado de: ', 'Analista', 1);
+insert into role (description, name, rank) values ('Encargado de: ', 'Jefe de Departamento de Desarrollo e Innovación Curricular', 3);
+insert into role (description, name, rank) values ('Encargado de: ', 'Jefe de Innovación Educativa', 2);
+insert into role (description, name, rank) values ('Encargado de: ', 'Jefe de la División de Innovacion Académica', 4);
+insert into role (description, name, rank) values ('Encargado de: plan de estudios,unidades de aprendizaje y programa sintético', 'Docente', 1);
---
+++ src/main/resources/import_data/import_studyPlanModes.sql
@@ -0,0 +1,4 @@
+-- add study_plan_mode
+insert into study_plan_mode (description, name) values ('Escolarizado', 'Escolarizado');
+insert into study_plan_mode (description, name) values ('A distancia', 'A distancia');
+insert into study_plan_mode (description, name) values ('Mixto', 'Mixto');
