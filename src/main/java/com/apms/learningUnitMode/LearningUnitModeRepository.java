f1cc30a2d46f3bc4afcac8b65450cfd484167102
akotadi <manuel_7795@hotmail.com>
2018-11-21 18:45:41 -0600

    Changing modality

--- src/main/java/com/apms/learningUnitMode/LearningUnitModeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.apms.learningUnitMode;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LearningUnitModeRepository extends JpaRepository<LearningUnitMode, Integer> {
-}
--- src/main/resources/import_data/import_studyPlanModes.sql
+++
@@ -1,4 +0,0 @@
--- add study_plan_mode
-insert into study_plan_mode (description, name) values ('Escolarizado', 'Escolarizado');
-insert into study_plan_mode (description, name) values ('A distancia', 'A distancia');
-insert into study_plan_mode (description, name) values ('Mixto', 'Mixto');
