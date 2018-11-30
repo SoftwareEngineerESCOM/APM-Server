9204f7cc245d9492b607b6c6cde9b638839d68e9
DavidJChavez <davidjchavez98@gmail.com>
2018-11-30 01:23:48 -0600

    Message 'No hay recursos humanos con ese cargo' added

--- src/main/java/com/apms/humanResource/HumanResourceRestController.java
+++ src/main/java/com/apms/humanResource/HumanResourceRestController.java
@@ -20,141 +20,151 @@ import com.apms.rest.RESTResponse;
 @RequestMapping("/humanResource")
 public class HumanResourceRestController {
 
-	@Autowired
-	private HumanResourceService humanResourceService;
+    @Autowired
+    private HumanResourceService humanResourceService;
 
-	/*
-	 ** Return a listing of all the resources
-	 */
-	@GetMapping
-	public RESTResponse<List<HumanResource>> getAll() {
-		List<HumanResource> res;
-		try {
-			res = humanResourceService.getAll();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new RESTResponse<List<HumanResource>>(RESTResponse.DBFAIL, "Inconsistencia en la base de datos.",
-					null);
-		}
-		if (!res.isEmpty()) {
-			return new RESTResponse<List<HumanResource>>(RESTResponse.OK, "", res);
-		} else {
-			return new RESTResponse<List<HumanResource>>(RESTResponse.FAIL,
-					"Servicios no disponibles.", null);
-		}
-	}
+    /*
+     ** Return a listing of all the resources
+     */
+    @GetMapping
+    public RESTResponse<List<HumanResource>> getAll() {
+        List<HumanResource> res;
+        try {
+            res = humanResourceService.getAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new RESTResponse<List<HumanResource>>(RESTResponse.DBFAIL, "Inconsistencia en la base de datos.",
+                    null);
+        }
+        if (!res.isEmpty()) {
+            return new RESTResponse<List<HumanResource>>(RESTResponse.OK, "", res);
+        } else {
+            return new RESTResponse<List<HumanResource>>(RESTResponse.FAIL,
+                    "Servicios no disponibles.", null);
+        }
+    }
 
-	/*
-	 ** Return one resource
-	 */
-	@GetMapping("/{id}")
-	public RESTResponse<HumanResource> getOne(@PathVariable Integer id) {
-		HumanResource res;
-		try {
-			res = humanResourceService.getOne(id);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new RESTResponse<HumanResource>(RESTResponse.DBFAIL, "Inconsistencia en la base de datos.", null);
-		}
-		if (res != null) {
-			return new RESTResponse<HumanResource>(RESTResponse.OK, "", res);
-		} else {
-			return new RESTResponse<HumanResource>(RESTResponse.FAIL, "Recurso humano no registrado.", null);
-		}
-	}
+    /*
+     ** Return one resource
+     */
+    @GetMapping("/{id}")
+    public RESTResponse<HumanResource> getOne(@PathVariable Integer id) {
+        HumanResource res;
+        try {
+            res = humanResourceService.getOne(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new RESTResponse<HumanResource>(RESTResponse.DBFAIL, "Inconsistencia en la base de datos.", null);
+        }
+        if (res != null) {
+            return new RESTResponse<HumanResource>(RESTResponse.OK, "", res);
+        } else {
+            return new RESTResponse<HumanResource>(RESTResponse.FAIL, "Recurso humano no registrado.", null);
+        }
+    }
 
-	/*
-	 ** Store a newly created resource in storage.
-	 */
-	@PostMapping
-	public RESTResponse<HumanResource> post(@RequestBody RESTRequest<HumanResource> humanResource) {
-		try {
-			if (humanResourceService.getOne(humanResource.getPayload().getId()) != null)
-				return new RESTResponse<HumanResource>(RESTResponse.FAIL, "El recurso humano ya existe en el sistema.",
-						null);
-			humanResourceService.add(humanResource.getPayload());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new RESTResponse<HumanResource>(RESTResponse.FAIL,
-					"Por el momento no se puede realizar el registro.", null);
-		}
-		return new RESTResponse<HumanResource>(RESTResponse.OK, "Registro finalizado exitosamente.", null);
-	}
+    /*
+     ** Store a newly created resource in storage.
+     */
+    @PostMapping
+    public RESTResponse<HumanResource> post(@RequestBody RESTRequest<HumanResource> humanResource) {
+        try {
+            if (humanResourceService.getOne(humanResource.getPayload().getId()) != null)
+                return new RESTResponse<HumanResource>(RESTResponse.FAIL, "El recurso humano ya existe en el sistema.",
+                        null);
+            humanResourceService.add(humanResource.getPayload());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new RESTResponse<HumanResource>(RESTResponse.FAIL,
+                    "Por el momento no se puede realizar el registro.", null);
+        }
+        return new RESTResponse<HumanResource>(RESTResponse.OK, "Registro finalizado exitosamente.", null);
+    }
 
-	/*
-	 ** Update the specified resource in storage partially.
-	 */
-	@PatchMapping
-	public RESTResponse<HumanResource> patch(@RequestBody RESTRequest<HumanResource> humanResource) {
-		try {
-			humanResourceService.update(humanResource.getPayload());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new RESTResponse<HumanResource>(RESTResponse.FAIL,
-					"Hubo un error al modificar. Por favor, intentelo mas tarde.", null);
-		}
-		return new RESTResponse<HumanResource>(RESTResponse.OK, "Los cambios se guardaron exitosamente.", null);
-	}
+    /*
+     ** Update the specified resource in storage partially.
+     */
+    @PatchMapping
+    public RESTResponse<HumanResource> patch(@RequestBody RESTRequest<HumanResource> humanResource) {
+        try {
+            humanResourceService.update(humanResource.getPayload());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new RESTResponse<HumanResource>(RESTResponse.FAIL,
+                    "Hubo un error al modificar. Por favor, intentelo mas tarde.", null);
+        }
+        return new RESTResponse<HumanResource>(RESTResponse.OK, "Los cambios se guardaron exitosamente.", null);
+    }
 
-	/*
-	 ** Update the specified resource in storage.
-	 */
-	@PutMapping
-	public RESTResponse<HumanResource> put(@RequestBody RESTRequest<HumanResource> humanResource) {
-		try {
-			humanResourceService.update(humanResource.getPayload());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new RESTResponse<HumanResource>(RESTResponse.FAIL,
-					"Hubo un error al modificar. Por favor, intentelo mas tarde.", null);
-		}
-		return new RESTResponse<HumanResource>(RESTResponse.OK, "Los cambios se guardaron exitosamente.", null);
-	}
+    /*
+     ** Update the specified resource in storage.
+     */
+    @PutMapping
+    public RESTResponse<HumanResource> put(@RequestBody RESTRequest<HumanResource> humanResource) {
+        try {
+            humanResourceService.update(humanResource.getPayload());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new RESTResponse<HumanResource>(RESTResponse.FAIL,
+                    "Hubo un error al modificar. Por favor, intentelo mas tarde.", null);
+        }
+        return new RESTResponse<HumanResource>(RESTResponse.OK, "Los cambios se guardaron exitosamente.", null);
+    }
 
-	/*
-	 ** Remove the specified resource from storage.
-	 */
-	@DeleteMapping("/{id}")
-	public RESTResponse<HumanResource> delete(@PathVariable Integer id) {
-		try {
+    /*
+     ** Remove the specified resource from storage.
+     */
+    @DeleteMapping("/{id}")
+    public RESTResponse<HumanResource> delete(@PathVariable Integer id) {
+        try {
+            humanResourceService.delete(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new RESTResponse<HumanResource>(RESTResponse.FAIL,
+                    "Por el momento no se puede realizar el registro.", null);
+        }
+        return new RESTResponse<HumanResource>(RESTResponse.OK, "Los cambios se guardaron exitosamente.", null);
+    }
 
-			humanResourceService.delete(id);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new RESTResponse<HumanResource>(RESTResponse.FAIL,
-					"Por el momento no se puede realizar el registro.", null);
-		}
-		return new RESTResponse<HumanResource>(RESTResponse.OK, "Los cambios se guardaron exitosamente.", null);
-	}
+    @GetMapping("/humanResourcesByWorkplaceIdAndPositionId/{idW}/{idP}")
+    public RESTResponse<List<HumanResource>> getHumanResourcesByWorkplaceIdAndPositionId(@PathVariable Integer idW,
+                                                                                         @PathVariable Integer idP) {
+        List<HumanResource> res;
+        try {
+            res = humanResourceService.getHumanResourcesByWorkplaceIdAndPositionId(idW, idP);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new RESTResponse<List<HumanResource>>(RESTResponse.DBFAIL, "Inconsistencia en la base de datos.", null);
+        }
+        if (!res.isEmpty()) {
+            return new RESTResponse<List<HumanResource>>(RESTResponse.OK, "", res);
+        } else {
+            return new RESTResponse<List<HumanResource>>(RESTResponse.FAIL, "No hay recursos humanos con ese cargo", res);
+        }
+    }
 
-	@GetMapping("/humanResourcesByWorkplaceIdAndPositionId/{idW}/{idP}")
-	public RESTResponse<List<HumanResource>> getHumanResourcesByWorkplaceIdAndPositionId(@PathVariable Integer idW,
-			@PathVariable Integer idP) {
-		return new RESTResponse<List<HumanResource>>(RESTResponse.OK, "",
-				humanResourceService.getHumanResourcesByWorkplaceIdAndPositionId(idW, idP));
-	}
+    @GetMapping("/humanResourcesByWorkplaceId/{id}")
+    public RESTResponse<List<HumanResource>> getHumanResourcesByWorkplaceId(@PathVariable Integer id) {
+        return new RESTResponse<List<HumanResource>>(RESTResponse.OK, "",
+                humanResourceService.getHumanResourcesByWorkplaceId(id));
+    }
 
-	@GetMapping("/humanResourcesByWorkplaceId/{id}")
-	public RESTResponse<List<HumanResource>> getHumanResourcesByWorkplaceId(@PathVariable Integer id) {
-		return new RESTResponse<List<HumanResource>>(RESTResponse.OK, "",
-				humanResourceService.getHumanResourcesByWorkplaceId(id));
-	}
-
-	@GetMapping("/humanResourceByName/{name}/{first_surname}/{second_surname}")
-	public RESTResponse<HumanResource> humanResourceByName(@PathVariable String name,
-			@PathVariable String first_surname, @PathVariable String second_surname) {
-		HumanResource res;
-		try {
-			res = humanResourceService.getOne(name, first_surname, second_surname);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new RESTResponse<HumanResource>(RESTResponse.DBFAIL, "Inconsistencia en la base de datos.", null);
-		}
-		if (res != null) {
-			return new RESTResponse<HumanResource>(RESTResponse.OK, "", res);
-		} else {
-			return new RESTResponse<HumanResource>(RESTResponse.FAIL, "Recurso Humano no registrado.", null);
-		}
-	}
+    @GetMapping("/humanResourceByName/{name}/{first_surname}/{second_surname}")
+    public RESTResponse<HumanResource> humanResourceByName(@PathVariable String name,
+                                                           @PathVariable String first_surname,
+                                                           @PathVariable String second_surname) {
+        HumanResource res;
+        try {
+            res = humanResourceService.getOne(name, first_surname, second_surname);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new RESTResponse<HumanResource>(RESTResponse.DBFAIL, "Inconsistencia en la base de datos.", null);
+        }
+        if (res != null) {
+            return new RESTResponse<HumanResource>(RESTResponse.OK, "", res);
+        } else {
+            return new RESTResponse<HumanResource>(RESTResponse.FAIL, "Recurso Humano no registrado.", null);
+        }
+    }
 
 }
