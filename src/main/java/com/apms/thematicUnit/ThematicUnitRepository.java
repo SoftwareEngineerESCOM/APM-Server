94a29fd06c6b82108d485d331d4f4738f05e8171
Manuel CH <manuel_7795@hotmail.com>
2018-12-02 20:50:45 -0600

    Adding List to Repository

--- src/main/java/com/apms/thematicUnit/ThematicUnitRepository.java
+++ src/main/java/com/apms/thematicUnit/ThematicUnitRepository.java
@@ -1,5 +1,7 @@
 package com.apms.thematicUnit;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
