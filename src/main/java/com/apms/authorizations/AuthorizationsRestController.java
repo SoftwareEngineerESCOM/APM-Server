b8c86c9b5a7a2967cb7fc1c82d437e1085b320b2
DavidJChavez <davidjchavez98@gmail.com>
2018-12-04 13:31:25 -0600

    Pues ya tienes lo de authorizations

---
+++ src/main/java/com/apms/authorizations/AuthorizationsRestController.java
@@ -0,0 +1,121 @@
+package com.apms.authorizations;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apms.rest.RESTRequest;
+import com.apms.rest.RESTResponse;
+
+@RestController
+@RequestMapping("/authorizations")
+public class AuthorizationsRestController {
+	
+    @Autowired
+    private AuthorizationsService authorizationsService;
+
+    /*
+    **Return a listing of all the resources
+    */
+    @GetMapping
+	public RESTResponse<List<Authorizations>> getAll() {
+		List<Authorizations> res;
+		try {
+			res = authorizationsService.getAll();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new RESTResponse<List<Authorizations>>(RESTResponse.DBFAIL, "Inconsistencia en la base de datos.", null);
+		}
+		if (!res.isEmpty()) {
+			return new RESTResponse<List<Authorizations>>(RESTResponse.OK, "", res);
+		} else {
+			return new RESTResponse<List<Authorizations>>(RESTResponse.FAIL, "Los catalogos necesarios no se han cargado.", null);
+		}
+	}
+
+    /*
+    **Return one resource
+    */
+    @GetMapping("/{id}")
+    public RESTResponse<Authorizations> getOne(@PathVariable Integer id) {
+        Authorizations res;
+        try{
+            res = authorizationsService.getOne(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new RESTResponse<Authorizations>(RESTResponse.DBFAIL, "Inconsistencia en la base de datos.", null);
+        }
+        if (res != null) {
+			return new RESTResponse<Authorizations>(RESTResponse.OK, "", res);
+		} else {
+			return new RESTResponse<Authorizations>(RESTResponse.FAIL, "Authorizations no registrado.", null);
+		}
+    }
+
+    /*
+    **Store a newly created resource in storage.
+    */
+    @PostMapping
+    public RESTResponse<Authorizations> post(@RequestBody RESTRequest<Authorizations> authorizations) {
+        try {
+            if(authorizationsService.getOne(authorizations.getPayload().getId()) != null)
+                return new RESTResponse<Authorizations>(RESTResponse.FAIL, "Authorizations ya existe en el sistema.", null);
+            authorizationsService.add(authorizations.getPayload());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new RESTResponse<Authorizations>(RESTResponse.FAIL, "Hubo un error en el registro. Por favor, intentelo mas tarde.", null);
+        }
+        return new RESTResponse<Authorizations>(RESTResponse.OK, "Registro finalizado exitosamente.", null);
+    }
+
+    /*
+    **Update the specified resource in storage partially.
+    */
+    @PatchMapping
+    public RESTResponse<Authorizations> patch(@RequestBody RESTRequest<Authorizations> authorizations) {
+        try {
+            authorizationsService.update(authorizations.getPayload());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new RESTResponse<Authorizations>(RESTResponse.FAIL, "Hubo un error al modificar. Por favor, intentelo mas tarde.", null);
+        }
+        return new RESTResponse<Authorizations>(RESTResponse.OK, "Authorizations modificado.", null);
+    }
+
+    /*
+    **Update the specified resource in storage.
+    */
+    @PutMapping
+    public RESTResponse<Authorizations> put(@RequestBody RESTRequest<Authorizations> authorizations) {
+        try {
+            authorizationsService.update(authorizations.getPayload());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new RESTResponse<Authorizations>(RESTResponse.FAIL, "Hubo un error al modificar. Por favor, intentelo mas tarde.", null);
+        }
+        return new RESTResponse<Authorizations>(RESTResponse.OK, "Authorizations modificado.", null);
+    }
+
+    /*
+    **Remove the specified resource from storage.
+    */
+    @DeleteMapping("/{id}")
+    public RESTResponse<Authorizations> delete(@PathVariable Integer id) {
+        try {
+            authorizationsService.delete(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new RESTResponse<Authorizations>(RESTResponse.FAIL, "Hubo un error en el registro. Por favor, intentelo mas tarde.", null);
+        }
+        return new RESTResponse<Authorizations>(RESTResponse.OK, "Authorizations modificado.", null);
+    }
+}
---
+++ src/main/java/com/apms/authorizations/AuthorizationsService.java
@@ -0,0 +1,37 @@
+package com.apms.authorizations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationsService {
+
+    @Autowired
+    private AuthorizationsRepository authorizationsRepository;
+
+    public List<Authorizations> getAll() {
+        List<Authorizations> records = new ArrayList<>();
+        authorizationsRepository.findAll().forEach(records::add);
+        return records;
+    }
+
+    public Authorizations getOne(Integer id) {
+	return authorizationsRepository.findById(id).isPresent() ? authorizationsRepository.findById(id).get() : null;
+    }
+
+    public void add(Authorizations authorizations) {
+        authorizationsRepository.save(authorizations);
+    }
+
+    public void update(Authorizations authorizations) {
+        // if exists updates otherwise inserts
+        authorizationsRepository.save(authorizations);
+    }
+
+    public void delete(Integer id) {
+        authorizationsRepository.deleteById(id);
+    }
+}
