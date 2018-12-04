ab77b4324cee12b65250affeb8995d2c9366af02
searleser97 <serchgabriel97@gmail.com>
2018-12-04 17:50:21 -0600

    updated: 04/12/2018 05:50:21 PM

---
+++ src/main/java/com/apms/learningUnitTask/LearningUnitTaskService.java
@@ -0,0 +1,37 @@
+package com.apms.learningUnitTask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LearningUnitTaskService {
+
+    @Autowired
+    private LearningUnitTaskRepository learningUnitTaskRepository;
+
+    public List<LearningUnitTask> getAll() {
+        List<LearningUnitTask> records = new ArrayList<>();
+        learningUnitTaskRepository.findAll().forEach(records::add);
+        return records;
+    }
+
+    public LearningUnitTask getOne(Integer id) {
+	return learningUnitTaskRepository.findById(id).isPresent() ? learningUnitTaskRepository.findById(id).get() : null;
+    }
+
+    public void add(LearningUnitTask learningUnitTask) {
+        learningUnitTaskRepository.save(learningUnitTask);
+    }
+
+    public void update(LearningUnitTask learningUnitTask) {
+        // if exists updates otherwise inserts
+        learningUnitTaskRepository.save(learningUnitTask);
+    }
+
+    public void delete(Integer id) {
+        learningUnitTaskRepository.deleteById(id);
+    }
+}
