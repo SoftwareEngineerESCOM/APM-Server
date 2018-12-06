2a2f8f69429dbe53ae6f6de4ee102cd073951005
searleser97 <serchgabriel97@gmail.com>
2018-12-05 19:38:18 -0600

    updated: 05/12/2018 07:38:18 PM

--- src/main/java/com/apms/learningUnit/LearningUnit.java
+++ src/main/java/com/apms/learningUnit/LearningUnit.java
@@ -1,71 +1,62 @@
 package com.apms.learningUnit;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
 import com.apms.academy.Academy;
 import com.apms.formationArea.FormationArea;
-import com.apms.semester.Semester;
 import com.apms.learningUnitStatus.LearningUnitStatus;
-
+import com.apms.semester.Semester;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.persistence.*;
 
 @Getter
 @Setter
 @Entity
 public class LearningUnit {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	@Column(nullable = false)
-	private String name;
-	@Column(nullable = false)
-	private Double TEPICCredits;
-	@Column(nullable = false)
-	private Double SATCACredits;
-	@Column(nullable = false)
-	private Double theoryHoursPerWeek;
-	@Column(nullable = false)
-	private Double practiceHoursPerWeek;
-	@ManyToOne
-	@JoinColumn(nullable = false)
-	private FormationArea formationArea;
-	@ManyToOne
-	@JoinColumn(nullable = false)
-	private Semester semester;
-<<<<<<< HEAD
-        @ManyToOne
-        @JoinColumn(nullable = false)
-        private LearningUnitStatus learningUnitStatus;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Double TEPICCredits;
+    @Column(nullable = false)
+    private Double SATCACredits;
+    @Column(nullable = false)
+    private Double theoryHoursPerWeek;
+    @Column(nullable = false)
+    private Double practiceHoursPerWeek;
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private FormationArea formationArea;
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Semester semester;
 
-=======
-	@ManyToOne
-	@JoinColumn
-	private Academy academy;
-	
->>>>>>> c9842e48771638c3dcae5db7b2749cd966fb8763
-	public LearningUnit() {
-	}
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private LearningUnitStatus learningUnitStatus;
 
-	public LearningUnit(Integer id, String name, Double tEPICCredits, Double sATCACredits, Double theoryHoursPerWeek,
-			Double practiceHoursPerWeek, FormationArea formationArea, Semester semester, Academy academy) {
-		this.id = id;
-		this.name = name;
-		TEPICCredits = tEPICCredits;
-		SATCACredits = sATCACredits;
-		this.theoryHoursPerWeek = theoryHoursPerWeek;
-		this.practiceHoursPerWeek = practiceHoursPerWeek;
-		this.formationArea = formationArea;
-		this.semester = semester;
-		this.academy = academy;
-	}
+    @ManyToOne
+    @JoinColumn
+    private Academy academy;
 
-	
+    public LearningUnit() {
+    }
+
+    public LearningUnit(Integer id, String name, Double tEPICCredits, Double sATCACredits, Double theoryHoursPerWeek,
+                        Double practiceHoursPerWeek, FormationArea formationArea, Semester semester, Academy academy) {
+        this.id = id;
+        this.name = name;
+        TEPICCredits = tEPICCredits;
+        SATCACredits = sATCACredits;
+        this.theoryHoursPerWeek = theoryHoursPerWeek;
+        this.practiceHoursPerWeek = practiceHoursPerWeek;
+        this.formationArea = formationArea;
+        this.semester = semester;
+        this.academy = academy;
+    }
+
+
 }
