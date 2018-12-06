0e6c4fa0f9e45df43250592176f0a27a30ba76e1
DoloresPGz <dolores.pgz@gmail.com>
2018-12-05 20:18:29 -0600

    insert learning unit task

---
+++ src/main/resources/import_data/import_learningUnitTask.sql
@@ -0,0 +1,6 @@
+-- add academic_level
+insert into learning_unit_task (learningUnit, user, startDate, deadLine) values ((select id from learning_unit limit 1), (select id from user limit 1), "13-12-2018", "24-12-2018");
+insert into learning_unit_task (learningUnit, user, startDate, deadLine) values ((select id from learning_unit limit 1 offset 1), (select id from user limit 1), "14-12-2018", "17-12-2018");
+insert into learning_unit_task (learningUnit, user, startDate, deadLine) values ((select id from learning_unit limit 1 offset 2), (select id from user limit 1), "17-12-2018", "20-12-2018");
+insert into learning_unit_task (learningUnit, user, startDate, deadLine) values ((select id from learning_unit limit 1 offset 3), (select id from user limit 1), "09-12-2018", "11-12-2018");
+insert into learning_unit_task (learningUnit, user, startDate, deadLine) values ((select id from learning_unit limit 1 offset 4), (select id from user limit 1), "11-12-2018", "13-12-2018");
