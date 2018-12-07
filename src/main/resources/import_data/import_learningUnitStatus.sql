5cdd087fdd4c8c6e99cde547220add2ce2bf78ae
DoloresPGz <dolores.pgz@gmail.com>
2018-12-07 01:52:00 -0600

    cambio de letra

--- src/main/resources/import_data/import_learningUnitStatus.sql
+++ src/main/resources/import_data/import_learningUnitStatus.sql
@@ -1,5 +1,5 @@
 -- add learning_Unit
-insert into learning_unit_status (id, description, name) values (1, 'Ya hay un usuario asignado a la unidad de aprendiaje', 'Asignado');
+insert into learning_unit_status (id, description, name) values (1, 'Ya hay un usuario asignado a la unidad de aprendiaje', 'Asignada');
 insert into learning_unit_status (id, description, name) values (2, 'No hay un usuario asignado a la unidad de aprendizaje', 'Sin asignar');
 insert into learning_unit_status (id, description, name) values (3, 'Hay un usuario trabajando en esta unidad de aprendizaje', 'En proceso');
 insert into learning_unit_status (id, description, name) values (4, 'La unidad de aprenidizaje ha sido aprobada', 'Finalizado');
