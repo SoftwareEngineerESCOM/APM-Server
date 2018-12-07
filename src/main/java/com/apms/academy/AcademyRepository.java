aa4166fad4269dd1ff8b8b3ea61c07fd277968e5
akotadi <manuel_7795@hotmail.com>
2018-12-06 19:12:07 -0600

    Fixing academy

--- src/main/java/com/apms/academy/AcademyRepository.java
+++ src/main/java/com/apms/academy/AcademyRepository.java
@@ -1,9 +1,14 @@
 package com.apms.academy;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 @Repository
-public interface AcademyRepository extends JpaRepository<Academy, Integer>{
+public interface AcademyRepository extends 
+JpaRepository<Academy, Integer> {
 
 }
