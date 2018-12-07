cb716394e0a10d8bd619c8adc2a0484d443e0eb8
DoloresPGz <dolores.pgz@gmail.com>
2018-12-07 03:13:01 -0600

    insert

--- src/main/java/com/apms/learningUnitStatus/LearningUnitStatus.java
+++ src/main/java/com/apms/learningUnitStatus/LearningUnitStatus.java
@@ -11,7 +11,6 @@ import javax.persistence.*;
 public class LearningUnitStatus {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(nullable = false)
     private String name;
