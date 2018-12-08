b57478bc77ac5cbe81fd5c9abab46c984ea392c5
akotadi <manuel_7795@hotmail.com>
2018-12-08 11:24:26 -0600

    Fixing getThematicUnitByLearningUnitId

--- src/main/java/com/apms/thematicUnit/ThematicUnitRepository.java
+++ src/main/java/com/apms/thematicUnit/ThematicUnitRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface ThematicUnitRepository extends JpaRepository<ThematicUnit, Integer> {
-	@Query(value = "SELECT * FROM thematic_unit WHERE content_id IN (SELECT id FROM content WHERE id = (SELECT id FROM synthetic_program WHERE learning_unit_id = :id))", nativeQuery = true)
+	@Query(value = "SELECT * FROM thematic_unit WHERE learning_unit_id = :id", nativeQuery = true)
 	List<ThematicUnit> getThematicUnitByLearningUnitId(@Param("id") Integer id);
 
 }
