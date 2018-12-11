06d1f023adb74bcd1c1d0c22d4f3be7cc569b155
akotadi <manuel_7795@hotmail.com>
2018-12-11 16:22:06 -0600

    Fixing a lot

--- src/main/java/com/apms/authorizations/AuthorizationsRepository.java
+++ src/main/java/com/apms/authorizations/AuthorizationsRepository.java
@@ -8,8 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 @Repository
-public interface AuthorizationsRepository extends 
-JpaRepository<Authorizations, Integer> {
-@Query("select authorizations.* from authorizations, synthetic_program, learning_unit where :id = synthetic_program.learning_unit_id and synthetic_program.id = authorizations.synthetic_program_id ;")
-    Authorizations getAuthorizationsByLearningUnitId(@Param("id") Integer id);
+public interface AuthorizationsRepository extends JpaRepository<Authorizations, Integer> {
+	@Query(value = "SELECT a.* FROM authorizations a, synthetic_program sp WHERE a.synthetic_program_id = sp.id AND sp.learning_unit_id = :id", nativeQuery = true)
+	Authorizations getAuthorizationsByLearningUnitId(@Param("id") Integer id);
 }
